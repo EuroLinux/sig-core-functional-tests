f5c6d77e06ba35ea9ae7589078bb70bcc9e7ec93
Alex Baranowski <alex@euro-linux.com>
2018-10-11 11:32:53 +0200

    Fix release detection for X.XX realses (X.X worked fine)

--- tests/0_common/01_dist_release_check.sh
+++ tests/0_common/01_dist_release_check.sh
@@ -2,6 +2,6 @@
 # Just a check to determine full version (example 5.8) or just dist (example 6)
 # Author Alex Baranowski <aleksander.baranowski@yahoo.pl>
 
-export qa_releasever=$( cat /etc/system-release | grep -o "[0-9].[0-9]" )
+export qa_releasever=$( cat /etc/system-release | grep -o "[0-9].[0-9]*" )
 export qa_dist=$(echo $qa_releasever | cut -f1 -d.)
 echo "Determine Release $qa_dist and $qa_releasever"
