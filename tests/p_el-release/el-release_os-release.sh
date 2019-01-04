48a9ea1622f2d4119009e077f1cc14a99afb0132
Alex Baranowski <alex@euro-linux.com>
2019-01-04 14:53:07 +0100

    Fix os-release test for EL7

--- tests/p_el-release/el-release_os-release.sh
+++ tests/p_el-release/el-release_os-release.sh
@@ -5,14 +5,14 @@
 t_Log "Running $0 - /etc/os-release has correct ABRT string for EuroLinux 7"
 
 if [ "$el_ver" = "7" ];then
-  for string in NAME=\"EuroLinux\"  ID_LIKE=\"eurolinux scientific centos fedora\" ID=\"rhel\"
-  do 
+  for string in NAME=\"EuroLinux\"  ID_LIKE=\"rhel scientific centos fedora\" ID=\"eurolinux\"
+  do
     grep -q $string /etc/os-release
     if [ $? -ne "0" ];then
       t_Log "missing string $string in os-release file !"
       exit 1
     fi
-  done  
+  done
 else
   echo "Skipping for CentOS 5 and 6 ..." ; exit 0
 fi
