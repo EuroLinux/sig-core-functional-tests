6d63d33e8b046c81e5936230f311b6e673c60d1e
Alex Baranowski <alex@euro-linux.com>
2019-05-07 10:40:48 +0200

    Fix SHIM tests

--- tests/p_shim/01_shim_secureboot_signed.sh
+++ tests/p_shim/01_shim_secureboot_signed.sh
@@ -3,9 +3,17 @@
 
 t_Log "Running $0 -  Verifying that shim.efi is correctly signed with correct cert"
 
+
+
 if [[ "$el_ver" = "7" && "$arch" = "x86_64" ]] ; then
+  [ -e /boot/efi/EFI/redhat/shim.efi ] && BOOT_EFI="/boot/efi/EFI/redhat/shim.efi"
+  [ -e /boot/efi/EFI/centos/shim.efi ] && BOOT_EFI="/boot/efi/EFI/centos/shim.efi"
+  if [ -z "$BOOT_EFI" ]; then 
+      t_Log "WARNING: Cannot find proper shim.efi !"
+      exit 0
+  fi
   t_InstallPackage pesign shim
-  pesign --show-signature --in /boot/efi/EFI/centos/shim.efi|grep -q 'Microsoft Windows UEFI Driver Publisher'
+  pesign --show-signature --in $BOOT_EFI |grep -q 'Microsoft Windows UEFI Driver Publisher'
   t_CheckExitStatus $?
 else
   t_Log "previous versions than CentOS 7 - or not x86_64 arch - aren't using shim/secureboot ... skipping"
