2bd7768b107f9aa61506e80ccf00b97a5077923b
Alex Baranowski <alex@euro-linux.com>
2019-11-08 15:28:40 +0100

    Fix fbi problem 1

--- tests/0_common/00_centos_repos.sh
+++ tests/0_common/00_centos_repos.sh
@@ -4,6 +4,8 @@ if [ "$el_ver" -eq "8" ]; then
   t_Log "python not installed by default on .el8. SKIP"
   exit 0
 else
+   [ -e /etc/yum.repos.d/fbi.repo ] &&  rm -v /etc/yum.repos.d/fbi.repo
+   [ -e /etc/yum.repos.d/fbi-base.repo ] &&  rm -v /etc/yum.repos.d/fbi-base.repo
    python tests/0_common/000_centos_default_repos.py
 fi
 
