c48d3c59167d9174365120761dad22424c1f5147
Alex Baranowski <alex@euro-linux.com>
2019-11-13 18:18:16 +0100

    Try to fix sig-core-functinal for el6 and el7 2

--- tests/p_lynx/lynx_default_page_eurolinux_test.sh
+++ tests/p_lynx/lynx_default_page_eurolinux_test.sh
@@ -7,6 +7,10 @@ if [ "$el_ver" -eq "8" ]; then
   t_Log "Package lynx not available in default repos on el8 => SKIP"
   exit 0
 fi
+if [ "$el_ver" -eq "6" ]; then
+  t_Log "Skip for EL6"
+  exit 0
+fi
 
 
 lynx -dump  | grep "EuroLinux"  >/dev/null 2>&1
