3d489dccca41e616a6f763a73b1e90b149b079e7
Alex Baranowski <alex@euro-linux.com>
2019-12-31 13:22:27 +0100

    Fix squid tests for EL6

--- tests/p_squid/squid_test.sh
+++ tests/p_squid/squid_test.sh
@@ -6,6 +6,11 @@ t_Log "Running $0 - Squid test."
 URL="https://en.euro-linux.com/"
 CHECK_FOR="EuroLinux"
 
+if [ "$el_ver" -eq "6" ]; then
+    URL="https://www.google.com"
+    CHECK_FOR="Google"
+fi
+
 squidclient -T 2 ${URL} | grep "${CHECK_FOR}"  >/dev/null 2>&1
 
 t_CheckExitStatus $?
