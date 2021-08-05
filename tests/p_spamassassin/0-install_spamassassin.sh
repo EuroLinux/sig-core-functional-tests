5da00ff66ef64564268f408b4722ab898298ad7c
kh <kh@euro-linux.com>
2021-08-05 15:31:33 +0200

    skip spmassassin test

--- tests/p_spamassassin/0-install_spamassassin.sh
+++ tests/p_spamassassin/0-install_spamassassin.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 # Author: Athmane Madjoudj <athmanem@gmail.com>
 
-if [ "$el_ver" -eq "8" ]; then
-    t_Log "Install spamassassin on eurolinux 8"
-    sudo dnf install --setopt=AppStream.module_hotfixes=true -y spamassassin
-else
-    t_InstallPackage  spamassassin
-fi
+t_InstallPackage  spamassassin
+
