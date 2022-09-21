49d74bd355cc811e27d5151451f75b883d5cb7e1
Alex Baranowski <alex@euro-linux.com>
2022-09-21 20:14:09 +0200

    update el-release test for el6 using local EuroES repository

--- tests/p_el-release/el-release_el-base_repos.sh
+++ tests/p_el-release/el-release_el-base_repos.sh
@@ -21,7 +21,7 @@ channels=$(rhn-channel --list)
 if echo $channels | grep -q "el-server-${el_ver}"; then
     echo "Configured to use EuroMan"
 else
-    grep "name=ELS base repo" /etc/yum.repos.d/el-base.repo >/dev/null 2>&1
+    grep "BaseOS"  /etc/yum.repos.d/eurolinux-6.repo || grep "name=ELS base repo" /etc/yum.repos.d/el-base.repo >/dev/null 2>&1
 fi
 
 t_CheckExitStatus $?
