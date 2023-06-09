4efba7c362b2c0b03f4c9d755aa8bfb3a1f6586b
Alex Baranowski <aleksander.baranowski@yahoo.pl>
2023-06-09 12:57:34 +0200

    replace eurolinux with google for p_wget test, because el6

--- tests/p_wget/wget_test.sh
+++ tests/p_wget/wget_test.sh
@@ -6,8 +6,8 @@ t_Log "Running $0 - wget can access http-host and download index.html."
 FILE=/var/tmp/index.html
 
 
-CHECK_FOR="EuroLinux"
-URL="https://euro-linux.com"
+CHECK_FOR="Google"
+URL="https://google.com"
 
 t_Log "Querying ${URL}"
 wget -q --output-document=${FILE} ${URL}
